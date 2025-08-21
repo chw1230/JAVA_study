4f1df3f20055fceff83961defc05817cc6268661
chw1230 <choihw1230@naver.com>
2025-08-21 13:35:09 +0900

    자바 고급 1편 8. 생산자 소비자 문제1 공부 - 2

--- javaStudy_github/inflearnJAVA/java-adv1/thread/bounded/BoundedMain.java
+++ javaStudy_github/inflearnJAVA/java-adv1/thread/bounded/BoundedMain.java
@@ -13,12 +13,12 @@ public class BoundedMain {
         // 1. BoundedQueue 선택
 //        BoundedQueue queue = new BoundedQueueV1(2); // V1의 문제 -> 소비와 생산에서 공통적인? 대칭적인? 문제 발생
 //        BoundedQueue queue = new BoundedQueueV2(2); // V2의 문제 -> synchronized의 락으로 인한 문제
-        BoundedQueue queue = new BoundedQueueV3(2);
+        BoundedQueue queue = new BoundedQueueV3(2); // V3의 문제 -> notify( )의 비효율의 아쉬운 점 존재
 
 
         // 2. 생산자, 소비자 실행 순서 선택, 반드시 하나만 선택!
-//        producerFirst(queue); // 생산자 먼저 실행
-        consumerFirst(queue); // 소비자 먼저 실행
+        producerFirst(queue); // 생산자 먼저 실행
+//        consumerFirst(queue); // 소비자 먼저 실행
     }
 
     private static void producerFirst(BoundedQueue queue) {
--- javaStudy_github/inflearnJAVA/java-adv1/thread/bounded/BoundedQueueV3.java
+++ javaStudy_github/inflearnJAVA/java-adv1/thread/bounded/BoundedQueueV3.java
@@ -49,14 +49,6 @@ public class BoundedQueueV3 implements BoundedQueue {
         return data;
     }
 
-    /* 주의
-    어떤 스레드가 put 메서드를 사용하고 있다면 take 메서드도 사용할 수 없는 것
-    두 메서드에 synchronized 키워드가 붙어 있기 때문에!
-    메서드 진입 시에 락을 획득하게 됨! 이 락은 객체당 하나 이므로
-    어떤 스레드가 메서드를 사용 중이면 이미 락을 획득해서 사용중인 것!
-    다른 메서드를 실행하려면 해당 메서드의 작업이 종료되어야 함!
-     */
-
     @Override
     public String toString() {
         return queue.toString();
